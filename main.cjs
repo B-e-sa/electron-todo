e423c048a01bb8f70f5788b83b1d907fd537e986
João Antônio <101114950+B-e-sa@users.noreply.github.com>
2022-11-01 17:12:28 -0300

    add app compability when closed or openned

--- main.cjs
+++ main.cjs
@@ -1,5 +1,5 @@
 const { create } = require('domain')
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -7,8 +7,27 @@ const createWindow = () => {
         height: 600
     })
     win.loadURL("http://localhost:5173/")
+    win.on('close', (event) => {
+        if (app.quitting) {
+            win.null
+        } else {
+            event.preventDefault()
+            win.hide()
+        }
+    })
 }
 
 app.whenReady().then(() => {
     createWindow()
+    app.on("active", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow
+        }
+    })
+})
+
+app.on("window-all=closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
 })
