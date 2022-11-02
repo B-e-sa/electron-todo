9603d4ca7e9842f69c4516ee0f9045976395abb7
João Antônio <101114950+B-e-sa@users.noreply.github.com>
2022-11-02 12:23:09 -0300

    add todo title and content change

--- main.cjs
+++ main.cjs
@@ -1,10 +1,14 @@
 const { create } = require('domain')
 const { app, BrowserWindow } = require('electron')
+const path = require("path")
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preloader.js")
+        }
     })
     win.loadURL("http://localhost:5173/")
     win.on('close', (event) => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = (): JSX.Element => {
+  const todos: {
+    title: string,
+    content: string,
+    color: string
+  }[] = [{
+    title: "TITULO",
+    content: "CONTEUDO",
+    color: "yellow"
+  },
+  {
+    title: "titulo",
+    content: "conteudo",
+    color: "blue"
+  }]
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div id="notes">
+        {todos.map((item) => {
+          return (
+            <div className="note">
+              <input
+                type="text"
+                max={10}
+                defaultValue={item.title}
+                name="note title"
+                onChange={(e) => {
+                  item.title = e.target.value
+                }}
+              />
+              <textarea
+                defaultValue={item.content}
+                name="note content area"
+                cols={30}
+                rows={10}
+                onChange={(e) => {
+                  item.content = e.target.value
+                }}
+              ></textarea>
+            </div>
+          )
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
